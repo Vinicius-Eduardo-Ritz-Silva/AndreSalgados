5a8a5fda0a33b4e23e01d790c831370ae3d48a21
GAMASTI\vinicius.silva <vinicius.silva@moovefy.com.br>
2025-03-13 14:48:39 -0300

    feat(models-iniciais):Criação da Model Pedido e do campo Quantidade na Model Produto.

---
+++ AndreSalgados/Models/Pedido.cs
@@ -0,0 +1,11 @@
+﻿namespace AndreSalgados.Models
+{
+    public class Pedido : MainModel
+    {
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+
+        public Guid ClienteId { get; set; }
+        public virtual Clientes Cliente { get; set; }
+    }
+}
--- AndreSalgados/Models/Produtos.cs
+++ AndreSalgados/Models/Produtos.cs
@@ -5,6 +5,7 @@
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
 
         public Guid TipoId { get; set; }
         public virtual TipoProduto Tipo { get; set; }
