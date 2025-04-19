c77acace28d57eeb4b8b3c8d920599093a81dbce
ve167887 <ve167887@gmail.com>
2025-04-19 12:44:51 -0300

    feat(VR-Context): Criado todas as Interfaces atuais e iniciado a criacao dos Repositories.

---
+++ Infraestructure/Repositories/ClienteRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Core.Entities;
+using Application.Core.Interfaces.Repositories;
+using Infraestructure.Data;
+
+namespace Infraestructure.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly VrContext _context;
+
+        public ClienteRepository(VrContext context) 
+        {
+            _context = context;
+        }
+
+        //Metodos aqui
+    }
+}
