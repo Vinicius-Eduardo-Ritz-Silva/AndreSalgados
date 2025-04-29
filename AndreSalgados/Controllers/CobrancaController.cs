a87d71f058e3204f866d0840d88d746440f4fceb
ve167887 <ve167887@gmail.com>
2025-04-29 17:49:51 -0300

    feat(08-ViewCobrancaController): Iniciado configurações da CobrancaController.

--- AndreSalgados/Controllers/CobrancaController.cs
+++ AndreSalgados/Controllers/CobrancaController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Core.Entities;
+﻿using AndreSalgados.ViewModels;
+using Application.Core.Entities;
 using Application.Core.Interfaces.Repositories;
 using Infraestructure.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +28,26 @@ namespace AndreSalgados.Controllers
             var cobranca = await _cobrancaRepository.GetWithInclude(p => p.Cliente);
 
             return cobranca.OrderBy(co => co.Cliente.Nome);
+        }
+
+        [HttpPost]
+        public RetornoViewModel DefinirDataCobranca(Guid id, DateTime dataCobranca)
+        {
+            return new RetornoViewModel
+            {
+                Sucesso = false,
+                Mensagem = "Implementar método Definir Data Cobrança"
+            };
+        }
+
+        [HttpPost]
+        public RetornoViewModel MarcarComoPerdida(Guid id)
+        {
+            return new RetornoViewModel
+            {
+                Sucesso = false,
+                Mensagem = "Implementar método Marcar Como Perdida"
+            };
         }
     }
 }
