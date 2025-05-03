2dfc5edf7abe17481d3905e4e1b0cd8cb04f5828
ve167887 <ve167887@gmail.com>
2025-05-03 13:32:01 -0300

    feat(VRC): Iniciado configuracoes dos graficos.

--- AndreSalgados/Controllers/RelatorioController.cs
+++ AndreSalgados/Controllers/RelatorioController.cs
@@ -1,6 +1,21 @@
-﻿namespace AndreSalgados.Controllers
+﻿using Application.Core.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndreSalgados.Controllers
 {
-    public class RelatorioController
+    public class RelatorioController : Controller
     {
+        private readonly IRelatorioVendaRepository _relatorioVendaRepository;
+
+        public RelatorioController(IRelatorioVendaRepository relatorioVendaRepository)
+        {
+            _relatorioVendaRepository = relatorioVendaRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
     }
 }
